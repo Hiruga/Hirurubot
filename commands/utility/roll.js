ed2d584505efa4841db82a9ea22de13cd6adc3b5
Hiruga <hirugahasagui@gmail.com>
2026-08-27 21:48:11 -0300

    Operações matemáticas nas rolagens

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -10,23 +10,72 @@ module.exports = {
         .setDescription('O número de dados que você quer rolar ')
         .setRequired(true))
     .addIntegerOption(option => 
-        option.setAutocomplete(true)
+        option.setChoices(
+            { name: 'd4', value: 4 },
+            { name: 'd6', value: 6 },
+            { name: 'd8', value: 8 },
+            { name: 'd10', value: 10 },
+            { name: 'd12', value: 12 },
+            { name: 'd20', value: 20 },
+        )
         .setName('lados')
         .setDescription('O número de lados que o dado terá')
-        .setRequired(true)),
+        .setRequired(true))
+    .addStringOption(option =>
+        option.setChoices(
+            { name: '+', value: '+' },
+            { name: '-', value: '-' },
+            { name: '*', value: '*' },
+            { name: '/', value: '/' },
+        )
+        .setName('operacao')
+        .setDescription('Operação matemática atribuida à rolagem')
+        .setRequired(false))
+    .addIntegerOption(option =>
+        option.setName('valor')
+        .setDescription('Valor usado na operação')
+        .setRequired(false)),
 
     async execute(interaction) {
+        const op = interaction.options.getString('operacao');
+        const val = interaction.options.getInteger('valor');
         const quant = interaction.options.getInteger('quant');
         const lados = interaction.options.getInteger('lados');
-
+        let resultF;
         const roll = (max) => Math.floor(Math.random() * max) +1;
 
         const resultados = [];
         for (let i = 0; i < quant; i++){
             resultados.push(roll(lados));
         }
-           
+        let resLen = resultados.length;
+        let somaD = 0;
+        for (let i = 0; i < resLen; i++){
+           somaD = somaD + resultados[i];
+        }
+        if(op != null){
+            switch(op){
+                case '+':
+                    resultF = somaD + val;
+                    break;
+                case '-':
+                    resultF = somaD - val;
+                    break;
+                case '*':
+                    resultF = somaD * val;
+                    break;
+                case '/':
+                    resultF = somaD / val;
+                    break;
+            }
+        }
+        let text;
+        if(op != null && val != null){
+            text = '``'+resultF+'`` <- ['+resultados.join(', ')+'] '+quant+'d'+lados+' '+op+' '+val+'.';
+        } else {
+                text = '``'+somaD+'`` <- ['+resultados.join(', ')+'] '+quant+'d'+lados+'.';
+            }
         await interaction.reply(
-            `Você rodou ${quant}d${lados}: [${resultados.join(', ')}]`);
+            `${text}`)
     }
 }
